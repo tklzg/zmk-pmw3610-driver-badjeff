924dcd5ff8405d47ba47ddef131466f01cadd1eb
tasin <tklzg@naver.com>
2024-06-17 21:13:00 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -798,7 +798,7 @@ static int pwm3610_pm_action(const struct device *dev, enum pm_device_action act
         return 0;
     case PM_DEVICE_ACTION_SUSPEND:
         set_interrupt(dev, false);
-        gpio_remove_callback(config->irq_gpio.port, &data->irq_gpio_cb);
+        //gpio_remove_callback(config->irq_gpio.port, &data->irq_gpio_cb);
         gpio_pin_configure_dt(&config->cs_gpio, GPIO_OUTPUT_LOW);
         return 0;
     default:
