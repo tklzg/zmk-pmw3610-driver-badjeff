cec6305c1fc3e966a858f22b9e0e9639abf8b365
tasin <tklzg@naver.com>
2024-06-17 20:07:06 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -790,12 +790,17 @@ static const struct sensor_driver_api pmw3610_driver_api = {
 };
 
 static int pwm3610_pm_action(const struct device *dev, enum pm_device_action action) {
+    
+
+    const struct pixart_config *config = dev->config;
+    
+
     LOG_INF("pwm3610_pm_action !!!!!!!!!!!!!!!!!!!!!!");
     switch (action) {
     case PM_DEVICE_ACTION_RESUME:
         return 0;
     case PM_DEVICE_ACTION_SUSPEND:
-        spi_cs_ctrl(dev, false);
+        gpio_pin_set_dt(&config->cs_gpio, (int)0);
         return 0;
     default:
         return -ENOTSUP;
