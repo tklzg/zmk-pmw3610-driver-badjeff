08915f32ed29e1b4317ed981e5c7d95e6f9829a0
tasin <tklzg@naver.com>
2025-12-01 19:00:55 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -795,14 +795,16 @@ static int pwm3610_pm_action(const struct device *dev, enum pm_device_action act
     case PM_DEVICE_ACTION_RESUME:
     LOG_INF("PMW3610 Resume");
     LOG_ERR("PMW3610 Resume");
-    pmw3610_write_reg(dev, PMW3610_REG_POWER_UP_RESET, PMW3610_POWERUP_CMD_WAKEUP);
+    reg_write(dev, PMW3610_REG_POWER_UP_RESET, PMW3610_POWERUP_CMD_WAKEUP);
+
     //set_interrupt(dev, true);
         //gpio_pin_configure_dt(&config->irq_gpio, GPIO_INPUT);
         return 0;
     case PM_DEVICE_ACTION_SUSPEND:
     LOG_INF("PMW3610 Suspend");
     LOG_ERR("PMW3610 Suspend");
-    pmw3610_write_reg(dev, PMW3610_REG_SHUTDOWN, SHUTDOWN_ENABLE);
+    reg_write(dev, PMW3610_REG_SHUTDOWN, SHUTDOWN_ENABLE);
+
         //set_interrupt(dev, false);
         //gpio_pin_configure_dt(&config->cs_gpio, GPIO_OUTPUT_LOW);
         return 0;
