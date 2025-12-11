1ddbc624a2a0d7d198e8387257c60433b809b523
tasin <tklzg@naver.com>
2025-12-12 08:23:44 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -788,7 +788,7 @@ static const struct sensor_driver_api pmw3610_driver_api = {
     .attr_set = pmw3610_attr_set,
 };
 
-static int pwm3610_pm_action(const struct device *dev, enum pm_device_action action) {
+static int pmw3610_pm_action(const struct device *dev, enum pm_device_action action) {
     const struct pixart_config *config = dev->config;
     struct pixart_data *data = dev->data;
     switch (action) {
@@ -842,7 +842,7 @@ static int pwm3610_pm_action(const struct device *dev, enum pm_device_action act
         .x_input_code = DT_PROP(DT_DRV_INST(n), x_input_code),                                     \
         .y_input_code = DT_PROP(DT_DRV_INST(n), y_input_code),                                     \
     };                                                                                             \
-    PM_DEVICE_DT_INST_DEFINE(n, pwm3610_pm_action);                                                                                               \
+    PM_DEVICE_DT_INST_DEFINE(n, pmw3610_pm_action);                                                                                               \
     DEVICE_DT_INST_DEFINE(n, pmw3610_init, PM_DEVICE_DT_INST_GET(n), &data##n, &config##n, POST_KERNEL,                \
                           CONFIG_SENSOR_INIT_PRIORITY, &pmw3610_driver_api);
 
